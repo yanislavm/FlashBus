15bc3c1a2753e15a835e4873acabd099c059b9a7
Yanislav Mihaylov <yanislav.mihaylov@callsign.com>
2015-04-25 18:50:25 +0100

    Implementing tests Triggering sticky events during event registration

---
+++ eventbus/src/test/java/com/msagi/eventbus/EventBusTest.java
@@ -0,0 +1,281 @@
+package com.msagi.eventbus;
+
+import com.msagi.eventbus.events.TestEvent;
+import com.msagi.eventbus.events.TestEventWithData;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.Assert.assertNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * JUnit test for {@link EventBus}
+ * @author yanislav.mihaylov
+ */
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest= Config.NONE)
+public class EventBusTest {
+	private final EventBus mEventBus = new EventBus();
+	private final List<EventBus.IEvent> mReceivedEvents = new ArrayList<>();
+
+	@Test
+	public void registrationWithNull() {
+		mEventBus.register(null, null);
+	}
+
+	@Test
+	public void postNullEvent() {
+		mEventBus.post(null);
+	}
+
+	@Test
+	public void postNullStickyEvent() {
+		mEventBus.postSticky(null);
+	}
+
+	@Test
+	public void getStickyWithNullValue() {
+		assertNull(mEventBus.getStickyEvent(null));
+	}
+
+	@Test
+	public void unregisterWithNullEventHandler() {
+		mEventBus.unregister(null);
+	}
+
+	@Test
+	public void registerNewEventHandler() {
+		final EventBus.IEventHandler<TestEvent> eventHandler = new EventBus.IEventHandler<TestEvent>() {
+			@Override
+			public void onEvent(final TestEvent event) {
+				mReceivedEvents.add(event);
+			}
+		};
+
+		mEventBus.register(TestEvent.class, eventHandler);
+		mEventBus.post(new TestEvent());
+
+		runUiTasks();
+		assertEquals(1, mReceivedEvents.size());
+	}
+
+	@Test
+	public void duplicateEventHandlerRegistration() {
+		final EventBus.IEventHandler<TestEvent> eventHandler = new EventBus.IEventHandler<TestEvent>() {
+			@Override
+			public void onEvent(final TestEvent event) {
+				mReceivedEvents.add(event);
+			}
+		};
+
+		mEventBus.register(TestEvent.class, eventHandler);
+		mEventBus.register(TestEvent.class, eventHandler);
+		mEventBus.post(new TestEvent());
+
+		runUiTasks();
+		assertEquals(1, mReceivedEvents.size());
+	}
+
+	@Test
+	public void registerNewEventHandlerAfterGarbageCollection() {
+		mEventBus.register(TestEvent.class, new EventBus.IEventHandler<TestEvent>() {
+			@Override
+			public void onEvent(final TestEvent event) {
+				mReceivedEvents.add(event);
+			}
+		});
+
+		// We are triggering GC as the EventBus does keep a week reference to the event handlers.
+		// This is done in order to prevent possible memory leaks. So after the GC as there is no
+		// reference to the handler we have not to be able to find any events
+		System.gc();
+
+		mEventBus.post(new TestEvent());
+
+		runUiTasks();
+		assertEquals(0, mReceivedEvents.size());
+	}
+
+	@Test
+	public void subscribeForEventButTriggerAnotherOne() {
+		final EventBus.IEventHandler<TestEvent> eventHandler = new EventBus.IEventHandler<TestEvent>() {
+			@Override
+			public void onEvent(final TestEvent event) {
+				mReceivedEvents.add(event);
+			}
+		};
+
+		mEventBus.register(TestEvent.class, eventHandler);
+		mEventBus.post(new TestEventWithData("test"));
+
+		runUiTasks();
+		assertEquals(0, mReceivedEvents.size());
+	}
+
+	@Test
+	public void correctEventDelivery() {
+		final EventBus.IEventHandler<TestEvent> eventHandler = new EventBus.IEventHandler<TestEvent>() {
+			@Override
+			public void onEvent(final TestEvent event) {
+				mReceivedEvents.add(event);
+			}
+		};
+
+		final EventBus.IEventHandler<TestEventWithData> eventHandler2 = new EventBus.IEventHandler<TestEventWithData>() {
+			@Override
+			public void onEvent(final TestEventWithData event) {
+				mReceivedEvents.add(event);
+			}
+		};
+
+		mEventBus.register(TestEvent.class, eventHandler);
+		mEventBus.register(TestEventWithData.class, eventHandler2);
+		mEventBus.post(new TestEventWithData("test"));
+
+		runUiTasks();
+		assertEquals(1, mReceivedEvents.size());
+		final EventBus.IEvent receivedEvent = mReceivedEvents.get(0);
+		assertEquals(TestEventWithData.class, receivedEvent.getClass());
+		assertEquals("test", ((TestEventWithData)receivedEvent).getData());
+	}
+
+	@Test
+	public void multipleEventsDelivery() {
+		final EventBus.IEventHandler<TestEvent> eventHandler = new EventBus.IEventHandler<TestEvent>() {
+			@Override
+			public void onEvent(final TestEvent event) {
+				mReceivedEvents.add(event);
+			}
+		};
+
+		final EventBus.IEventHandler<TestEventWithData> eventHandler2 = new EventBus.IEventHandler<TestEventWithData>() {
+			@Override
+			public void onEvent(final TestEventWithData event) {
+				mReceivedEvents.add(event);
+			}
+		};
+
+		mEventBus.register(TestEvent.class, eventHandler);
+		mEventBus.register(TestEventWithData.class, eventHandler2);
+
+		mEventBus.post(new TestEvent());
+		mEventBus.post(new TestEventWithData("test"));
+
+		runUiTasks();
+		assertEquals(2, mReceivedEvents.size());
+	}
+
+	@Test
+	public void missingStickyEvent() {
+		final TestEvent event = mEventBus.getStickyEvent(TestEvent.class);
+		assertNull(event);
+	}
+
+	@Test
+	public void postStickyEvent() {
+		mEventBus.postSticky(new TestEvent());
+		final TestEvent event = mEventBus.getStickyEvent(TestEvent.class);
+		assertNotNull(event);
+	}
+
+	@Test
+	public void receiveStickyEvent() {
+		final EventBus.IEventHandler<TestEvent> eventHandler = new EventBus.IEventHandler<TestEvent>() {
+			@Override
+			public void onEvent(final TestEvent event) {
+				mReceivedEvents.add(event);
+			}
+		};
+
+		mEventBus.register(TestEvent.class, eventHandler);
+		mEventBus.postSticky(new TestEvent());
+
+		runUiTasks();
+		assertEquals(1, mReceivedEvents.size());
+		Assert.assertNotNull(mEventBus.getStickyEvent(TestEvent.class));
+	}
+
+	@Test
+	public void receiveStickyEventOnRegistration() {
+		final EventBus.IEventHandler<TestEvent> eventHandler = new EventBus.IEventHandler<TestEvent>() {
+			@Override
+			public void onEvent(final TestEvent event) {
+				mReceivedEvents.add(event);
+			}
+		};
+
+		mEventBus.postSticky(new TestEvent());
+		mEventBus.register(TestEvent.class, eventHandler);
+
+		runUiTasks();
+		assertEquals(1, mReceivedEvents.size());
+		Assert.assertNotNull(mEventBus.getStickyEvent(TestEvent.class));
+	}
+
+	@Test
+	public void unregisterEventHandler() {
+		final EventBus.IEventHandler<TestEvent> eventHandler = new EventBus.IEventHandler<TestEvent>() {
+			@Override
+			public void onEvent(final TestEvent event) {
+				mReceivedEvents.add(event);
+			}
+		};
+
+		mEventBus.register(TestEvent.class, eventHandler);
+		mEventBus.unregister(eventHandler);
+
+		mEventBus.post(new TestEvent());
+
+		runUiTasks();
+		assertEquals(0, mReceivedEvents.size());
+	}
+
+	@Test
+	public void unexpectedExceptionThrownFromEventHandler() {
+		final EventBus.IEventHandler<TestEvent> eventHandler = new EventBus.IEventHandler<TestEvent>() {
+			@Override
+			public void onEvent(final TestEvent event) {
+				throw new RuntimeException();
+			}
+		};
+
+		mEventBus.register(TestEvent.class, eventHandler);
+		mEventBus.post(new TestEvent());
+
+		runUiTasks();
+		assertEquals(0, mReceivedEvents.size());
+	}
+
+	@Test
+	public void unexpectedExceptionThrownFromEventHandlerDuringRegistration() {
+		final EventBus.IEventHandler<TestEvent> eventHandler = new EventBus.IEventHandler<TestEvent>() {
+			@Override
+			public void onEvent(final TestEvent event) {
+				throw new RuntimeException();
+			}
+		};
+
+		mEventBus.postSticky(new TestEvent());
+		mEventBus.register(TestEvent.class, eventHandler);
+
+		runUiTasks();
+		assertEquals(0, mReceivedEvents.size());
+	}
+
+	/**
+	 * Forces Robolectric to force Handler to deliver events to the UI thread
+	 */
+	private void runUiTasks() {
+		Robolectric.runUiThreadTasks();
+	}
+}
---
+++ eventbus/src/test/java/com/msagi/eventbus/events/TestEventWithData.java
@@ -0,0 +1,28 @@
+package com.msagi.eventbus.events;
+
+import com.msagi.eventbus.EventBus;
+
+/**
+ * Test event that contains also a data
+ * @author yanislav.mihaylov
+ */
+public class TestEventWithData implements EventBus.IEvent {
+	/** Test data field */
+	private String mData;
+
+	/**
+	 * Constructor
+	 * @param data Test data
+	 */
+	public TestEventWithData(final String data) {
+		mData = data;
+	}
+
+	/**
+	 * Gets the Test Data
+	 * @return Test Data
+	 */
+	public String getData() {
+		return mData;
+	}
+}
