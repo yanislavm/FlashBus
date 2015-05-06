f04217b595a7d51ef21e9455d04cb8999b7760c8
Miklos Sagi <miklos.sagi@gmail.com>
2015-05-06 10:10:32 +0100

    feature/performance_optimisation EventsHolder recycling to lighten GC

--- eventbus/src/main/java/com/msagi/eventbus/MessagesHandler.java
+++ eventbus/src/main/java/com/msagi/eventbus/MessagesHandler.java
@@ -8,90 +8,136 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Handles all the events that need to be fired on the Handler
+ *
  * @author yanislav.mihaylov
  */
 public class MessagesHandler {
-	/**
-	 * Tag for logging.
-	 */
-	private static final String TAG = MessagesHandler.class.getSimpleName();
+    /**
+     * Tag for logging.
+     */
+    private static final String TAG = MessagesHandler.class.getSimpleName();
 
-	/**
-	 * Holds the different Events with their event handlers
-	 */
-	private static class EventsHolder {
-		/**
-		 * Event Handler
-		 */
-		private EventBus.IEventHandler mEventHandler;
-		/**
-		 * Event
-		 */
-		private EventBus.IEvent mEvent;
+    /**
+     * Holds Event - Event handler pairs.
+     */
+    private static class EventsHolder {
 
-		/**
-		 * Constructor
-		 * @param event Event that needs to be triggered
-		 * @param eventHandler Event Handler that will be notified for the event
-		 */
-		private EventsHolder(EventBus.IEvent event, EventBus.IEventHandler eventHandler) {
-			mEvent = event;
-			mEventHandler = eventHandler;
-		}
-	}
+        /**
+         * Cache size.
+         */
+        private static final int CACHE_SIZE = 250;
 
-	/**
-	 * The events that needs to be triggered on the Handler.
-	 */
-	private ConcurrentLinkedQueue<EventsHolder> mEvents = new ConcurrentLinkedQueue<>();
+        /**
+         * Cache to recycle unused events holders and lighten GC.
+         */
+        private static final ConcurrentLinkedQueue<EventsHolder> CACHE = new ConcurrentLinkedQueue<>();
 
-	/**
-	 * Handler on which we will process the Events
-	 */
-	private final Handler mHandler;
+        /**
+         * Event Handler to deliver the event instance to.
+         */
+        private EventBus.IEventHandler mEventHandler;
 
-	/**
-	 * Instance of Runnable that will be used for the event delivery on the Handler
-	 */
-	private final Runnable mHandlerRunnable = new Runnable() {
-		@Override
-		public void run() {
-			EventsHolder eventHolder;
-			while ((eventHolder= mEvents.poll()) != null) {
-				try {
-					eventHolder.mEventHandler.onEvent(eventHolder.mEvent);
-				} catch (RuntimeException e) {
-					Log.e(TAG, "Error dispatching event", e);
-				}
-			}
-			mHandlerTriggered.set(false);
-		}
-	};
+        /**
+         * Event instance to deliver.
+         */
+        private EventBus.IEvent mEvent;
 
-	/**
-	 * Keeps the value if the Handler is triggered or not
-	 */
-	private final AtomicBoolean mHandlerTriggered = new AtomicBoolean(false);
+        /**
+         * Constructor
+         *
+         * @param event        Event that needs to be triggered
+         * @param eventHandler Event Handler that will be notified for the event
+         */
+        private EventsHolder(final EventBus.IEvent event, final EventBus.IEventHandler eventHandler) {
+            mEvent = event;
+            mEventHandler = eventHandler;
+        }
 
-	/**
-	 * Constructor
-	 * @param handler Handler on which we will handle the events
-	 */
-	protected MessagesHandler(final Handler handler) {
-		mHandler = handler;
-	}
+        /**
+         * Obtain new holder instance.
+         *
+         * @param event        The event instance.
+         * @param eventHandler The event handler instance.
+         * @return The holder containing the given event - event handler pair.
+         */
+        private static EventsHolder obtain(final EventBus.IEvent event, final EventBus.IEventHandler eventHandler) {
+            final EventsHolder cachedHolder = CACHE.poll();
+            if (cachedHolder == null) {
+                return new EventsHolder(event, eventHandler);
+            } else {
+                cachedHolder.mEvent = event;
+                cachedHolder.mEventHandler = eventHandler;
+                return cachedHolder;
+            }
+        }
 
-	/**
-	 * Posts events
-	 * @param event Event that needs to be posted
-	 * @param eventHandler Event handler that will handle the event
-	 * @param <T> Event type
-	 */
-	public <T extends EventBus.IEvent> void postEvent(final T event, final EventBus.IEventHandler<T> eventHandler) {
-		mEvents.add(new EventsHolder(event, eventHandler));
+        /**
+         * Recycle holder instance.
+         */
+        private void recycle() {
+            if (CACHE.size() < CACHE_SIZE) {
+                mEvent = null;
+                mEventHandler = null;
+                CACHE.add(this);
+            }
+        }
+    }
 
-		if (mHandlerTriggered.compareAndSet(/* Expected value */ false, /* New Value*/ true)) {
-			mHandler.post(mHandlerRunnable);
-		}
-	}
+    /**
+     * The events that needs to be triggered on the Handler.
+     */
+    private ConcurrentLinkedQueue<EventsHolder> mEvents = new ConcurrentLinkedQueue<>();
+
+    /**
+     * Handler on which we will process the Events
+     */
+    private final Handler mHandler;
+
+    /**
+     * Instance of Runnable that will be used for the event delivery on the Handler
+     */
+    private final Runnable mHandlerRunnable = new Runnable() {
+        @Override
+        public void run() {
+            EventsHolder eventHolder;
+            while ((eventHolder = mEvents.poll()) != null) {
+                try {
+                    eventHolder.mEventHandler.onEvent(eventHolder.mEvent);
+                    eventHolder.recycle();
+                } catch (RuntimeException e) {
+                    Log.e(TAG, "Error dispatching event", e);
+                }
+            }
+            mHandlerTriggered.set(false);
+        }
+    };
+
+    /**
+     * Keeps the value if the Handler is triggered or not
+     */
+    private final AtomicBoolean mHandlerTriggered = new AtomicBoolean(false);
+
+    /**
+     * Constructor
+     *
+     * @param handler Handler on which we will handle the events
+     */
+    protected MessagesHandler(final Handler handler) {
+        mHandler = handler;
+    }
+
+    /**
+     * Posts events
+     *
+     * @param event        Event that needs to be posted
+     * @param eventHandler Event handler that will handle the event
+     * @param <T>          Event type
+     */
+    public <T extends EventBus.IEvent> void postEvent(final T event, final EventBus.IEventHandler<T> eventHandler) {
+        mEvents.add(EventsHolder.obtain(event, eventHandler));
+
+        if (mHandlerTriggered.compareAndSet(/* Expected value */ false, /* New Value*/ true)) {
+            mHandler.post(mHandlerRunnable);
+        }
+    }
 }
